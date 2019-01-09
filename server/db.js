3633aca42dbc085091f16adb5d365f34a118d918
ggbandit <ggbandit@hotmail.com>
2019-01-09 15:53:17 +0700

    updated Auth mongoDB

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ var mongojs = require('mongojs');
 
 var databaseUrl = 'mongodb://localhost/hwData';
 var collections = ['temperature'];
-var option = {"auth": {"user": "tgr", "password": "tgr2019"} }
+var option = {"auth": {"user": "tgr2019", "password": "tgr2019"} }
 
 var connect = mongojs(databaseUrl, collections, option);
 
