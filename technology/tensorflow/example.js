51c42d06cd761ffbd462992b476cfc36a0f2dc15
tanakorn0314 <tanakorn0412@gmail.com>
2019-01-09 15:52:19 +0700

    predictable 1

--- technology/tensorflow/example.js
+++ technology/tensorflow/example.js
@@ -1,10 +1,12 @@
 const tf = require('@tensorflow/tfjs');
 require('@tensorflow/tfjs-node');
 
-//var csv = require("fast-csv");
+var csv = require("fast-csv");
 var xs = [];
 var ys = [];
-/*
+
+var data = [];
+
 function range(start, end) {
     var ans = [];
     for (let i = start; i < end; i++) {
@@ -14,61 +16,82 @@ function range(start, end) {
 }
 
 function readCSV() {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
         csv
-        .fromPath(file_name) 
-        .on("data", function(str){    
-        })
-        .on("end", function(){
-        
-        console.log(data.length);
-        resolve(data);
-        });
+            .fromPath("./data/THB.csv")
+            .on("data", function (str) {
+                data.push(str);
+            })
+            .on("end", function () {
+                resolve(data);
+            });
     });
 }
-*/
+
 
 async function prepareData() {
-    /*
-    MAX = -999;
+
+    await readCSV();
+
+    MAX = -999.0;
     const len = data.length
-    for (i=0; i<len; i++) {
-        if (MAX <= data[i]) {
-            MAX = data[i];
+    for (i = 0; i < len; i++) {
+        if (MAX <= data[i][1]) {
+            MAX = data[i][1];
         }
     }
-    
+
     let dataset = data.map((number) => {
-        return number/MAX;
+        return number[1]/MAX;
     })
+
+    const TIME_STEP = 3;
+    const NUM_OUT = 0;
 
     let arr = range(TIME_STEP, dataset.length - NUM_OUT + 1);
 
-    arr.forEach(function(i) {
-        
+    arr.forEach(function (i) {
+        let x = [];
+        // x = [data[i-3][0], data[i-2][0], data[i-1][0]];
+        for(j=i-TIME_STEP; j < i ;j++) {
+            dateMs = Date.parse(data[j][0]);
+            x.push(dateMs);
+        }
+        xs.push(x);
+
+        let sum = 0;
+        for(let j=i; j>i-TIME_STEP; j--){
+            sum += parseInt(data[j-1][1]);
+        }
+        let avg = sum/TIME_STEP;
+
+        ys.push(avg);
     });
-    */
-   xs = [[10,20,30],[20,30,40], ,[30,40,50]];
-   ys = [40,50,60];
-    
+
+    // console.log(xs);
+    // console.log(ys);
+    // xs = [[10, 20, 30], [20, 30, 40], [30, 40, 50]];
+    // ys = [40, 50, 60];
 }
 
 const model = tf.sequential();
 
-model.add(tf.layers.xxx({
-    units: xxx,
-    inputShape: [xxx],
-    returnSequences: xxx
+//input
+model.add(tf.layers.lstm({
+    units: 10,  // 50 node if more will slower
+    inputShape: [3, 1], // input 3 time steps each time step 1 dimension  e.g. [10,20,30]
+    returnSequences: false
 }));
 
-model.add(tf.layers.xxx({
-    units: xxx,
+//output
+model.add(tf.layers.dense({
+    units: 1,   // 1 output
     kernelInitializer: 'VarianceScaling',
-    activation: 'xxx'
+    activation: 'relu'
 }));
 
-const LEARNING_RATE = xxx;
-const optimizer = tf.train.xxx(LEARNING_RATE);
+const LEARNING_RATE = 0.001;
+const optimizer = tf.train.adam(LEARNING_RATE);
 
 model.compile({
     optimizer: optimizer,
@@ -76,31 +99,39 @@ model.compile({
     metrics: ['accuracy'],
 });
 
-async function main(){
-	async function trainModel(){
+async function main() {
+    async function trainModel() {
         const history = await model.fit(
             trainXS,
             trainYS,
             {
-                batchSize: xxx,
-                epochs: xxx,
-                shuffle:xxx,
-                validationSplit: xxx
+                batchSize: 1,   //bunch of data
+                epochs: 10,     //round of training
+                shuffle: true,      //random select data 
+                validationSplit: 0.2    // split 20 %, test 80 %
             });
     }
-    await prepareData();
-    
-    
-	await trainModel();
-    const saveResult = await model.save('file://xxx');
-    
+    // await prepareData();
+    // trainXS = tf.tensor2d(xs);
+    // trainXS = tf.reshape(trainXS, [-1, 3, 1])   //[numofdata, recursive round, dimen of feature of input]   -1 means any
+
+    // trainYS = tf.tensor1d(ys);  //model need 2 
+    // trainYs = tf.reshape(trainYS, [-1, 1])  //[num of result, num if feature output]
+
+    // await trainModel();
+    // await model.save('file://model');
+
     const load = async () => {
-        const model = await tf.loadModel('file://xxx');
-      };
-      
+        await tf.loadModel('file://model/model.json');
+    };
+
     load();
 
-    const r = model.predict(xxx);
+    testData = [[Date.parse("Nov 23, 2018"), Date.parse("Nov 25, 2018"), Date.parse("Nov 21, 2018")]];
+    testData = tf.tensor2d(testData);
+    testData = tf.reshape(testData, [-1, 3, 1]);
+
+    const r = model.predict(testData);
     let result = r.dataSync()[0];
     console.log(result);
 }
